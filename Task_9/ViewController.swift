3e3449514603effbfa0061fc982dbc4117cc24ef
PavelSav <kvartet300@ickoud.com>
2019-08-24 22:36:55 +0300

    Initial Commit

---
+++ Task_9/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Task_9
+//
+//  Created by MacAir300 on 8/24/19.
+//  Copyright © 2019 MacAir300. All rights reserved.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+    
+    @IBOutlet weak var button_7_9: UIButton!
+    @IBOutlet weak var button_7_5: UIButton!
+    @IBOutlet weak var view_7_9: UIView!
+    @IBOutlet weak var view_7_5: UIView!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
